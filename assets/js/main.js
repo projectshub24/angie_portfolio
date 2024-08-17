9476f79608f4d5bf85092c182ce720eb20934761
Cananea <143718951+Cananea@users.noreply.github.com>
2024-08-17 15:46:51 +0800

    3rd

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,7 +33,7 @@ function headerShadow() {
 
 /* ----- TYPING EFFECT ----- */
 var typingEffect = new Typed(".typedText",{
-  strings : ["Designer","Youtuber","Developer"],
+  strings : ["ESL Teacher","Administrator","Virtual Assistant"],
   loop : true,
   typeSpeed : 100, 
   backSpeed : 80,
